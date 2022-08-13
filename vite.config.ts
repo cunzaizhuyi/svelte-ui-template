319af59f45fe04f8ee66a13bea0e9906e41a20e3
cunzaizhuyi <877824709@qq.com>
2022-08-13 17:08:31 +0800

    fix: unocss配置问题

--- vite.config.ts
+++ vite.config.ts
@@ -5,35 +5,56 @@ import path from 'path';
 import { extractorSvelte } from '@unocss/core'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    Unocss({
-      extractors: [extractorSvelte],
-      mode: 'svelte-scoped',
-    }),
-    svelte({}),
-  ],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, '/src') },
-    ],
-  },
-  build: {
-    cssCodeSplit: true,
-    lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.ts'),
-      name: 'MyLib',
-      fileName: (format) => `index.${format}.js`
+export default defineConfig(({ command, mode }) => {
+  const common = {
+    resolve: {
+      alias: [
+        { find: '@', replacement: path.resolve(__dirname, '/src') },
+      ],
     },
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      // external: ['unocss'],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        // globals: {
-        //   unocss: 'unocss'
-        // }
+  };
+
+  if (command === 'serve') {
+    return {
+      plugins: [
+        Unocss({
+          extractors: [extractorSvelte],
+        }),
+        svelte({}),
+      ],
+      ...common,
+    }
+  }
+
+  if (command === 'build') {
+    return {
+      ...common,
+      plugins: [
+        Unocss({
+          extractors: [extractorSvelte],
+          mode: 'svelte-scoped',
+        }),
+        svelte({}),
+      ],
+      build: {
+        cssCodeSplit: true,
+        lib: {
+          entry: path.resolve(__dirname, 'src/lib/index.ts'),
+          name: 'MyLib',
+          fileName: (format) => `index.${format}.js`
+        },
+        rollupOptions: {
+          // 确保外部化处理那些你不想打包进库的依赖
+          // external: ['unocss'],
+          output: {
+            // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+            // globals: {
+            //   unocss: 'unocss'
+            // }
+          }
+        }
       }
     }
   }
+
 })
