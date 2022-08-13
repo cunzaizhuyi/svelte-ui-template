15413c1fea0339817e63db7959c648901f67c1c9
cunzaizhuyi <877824709@qq.com>
2022-08-13 15:00:21 +0800

    fix: 修改html

--- svelte.config.js
+++ svelte.config.js
@@ -3,5 +3,6 @@ import sveltePreprocess from 'svelte-preprocess'
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
+  preprocess: sveltePreprocess({
+  })
 }
